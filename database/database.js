bde62e70e79b50c3fb5f03681412a065d8001302
ushagour <ali.ouchagour01@gmail.com>
2025-05-18 12:52:13 +0100

    refactor: add configuration files for development and production environments; update database connection settings and asset URLs

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './db.sqlite', // SQLite database file
+    storage: './database/development.sqlite', // SQLite database file
     logging: false, // Disable logging for cleaner output
 });
 
