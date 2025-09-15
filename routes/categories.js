ca21e153422543827f54e88bbf7c3fbeedfcffaa
ushagour <ali.ouchagour01@gmail.com>
2025-09-15 23:29:53 +0100

    feat: implimenting category endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Category = require("../models/Category");
+const { Listing } = require("../models");
 
 const router = express.Router();
 
@@ -16,8 +17,68 @@ router.get("/:id", async(req, res) => {
   const id = req.params.id;
 
   try {
-    const category = await Category.findByPk(id);
+    const category = await Category.findByPk(id,{
+         include: [
+        {
+          model: Listing,
+          attributes: ['id'], // Include only the file_name attribute
+        }
+        
+      ]}
+    );
+
     res.status(200).json(category);
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/", async(req, res) => {
+  const { name, 	icon } = req.body;
+console.log("Creating new category:", { name, 	icon }); // Log the category being created
+  if (!name || !icon) {
+    return res.status(400).json({ error: "Name and icon are required" });
+  }
+
+  try {
+    const newCategory = await Category.create({ name, icon });
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+);
+
+router.put("/:id", async(req, res) => {
+  const id = req.params.id;
+  const { name, description,icon } = req.body;
+
+  try {
+    const category = await Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+    category.name = name;
+    category.description = description;
+    category.icon = icon;
+    await category.save();
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async(req, res) => {
+  const id = req.params.id;
+
+  try {
+    const category = await Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+    await category.destroy();
+    res.status(204).send();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
